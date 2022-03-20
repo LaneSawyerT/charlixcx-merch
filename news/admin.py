f8aed78d8cd91309d39833ddf635ba29f0b54b76
LaneSawyerT <Lanefair@gmail.com>
2022-03-20 21:27:27 +0000

    Added model, views and admin functionality for news

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import NewsPost
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+@admin.register(NewsPost)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('post_title', 'slug', 'created_on')
+    search_fields = ['post_title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('post_title',)}
+    summernote_fields = ('content',)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class NewsPost(models.Model):
+    """
+    Where the news post is created
+    """
+    post_title = models.CharField(max_length=40, unique=True)
+    slug = models.SlugField(max_length=40, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="news_posts"
+    )
+    featured_image = CloudinaryField('image', default='placeholder')
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='news_like', blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.post_title
+
+    def number_of_likes(self):
+        return self.likes.count()
