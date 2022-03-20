18fe7c45599fab5cf65dbf3aba73c33cc2f3e524
LaneSawyerT <Lanefair@gmail.com>
2022-03-20 01:13:57 +0000

    Initial commit and deployment. Created first app called news

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn charlixcx_merch.wsgi
---
+++ charlixcx_merch/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for charlixcx_merch project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'charlixcx_merch.settings')
+
+application = get_asgi_application()
