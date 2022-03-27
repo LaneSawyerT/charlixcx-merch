eb768fea836f068b307f165df552ef4104c44948
LaneSawyerT <Lanefair@gmail.com>
2022-03-27 13:00:06 +0000

    Added stripelement js and checkout css to show when info is wrong for checkout.html

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KaZBmJ1ScGHD31vF2edch8lXJEAxkBeXYlmLko8xw6jHGZyh56jd1Gq0xIWsGDZgOASIyQVhZLJlkwKdLtMDR0X007cItjYkT',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
