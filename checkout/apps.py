4e63076d79d659ec7c89f3d71f4f13228e30dbfa
LaneSawyerT <Lanefair@gmail.com>
2022-03-27 17:08:11 +0000

    Added checkout_success.httml and logic

--- checkout/apps.py
+++ checkout/apps.py
@@ -6,11 +6,3 @@ class CheckoutConfig(AppConfig):
 
     def ready(self):
         import checkout.signals
-
-
-#from django.apps import AppConfig
-
-
-#class CheckoutConfig(AppConfig):
-#    default_auto_field = 'django.db.models.BigAutoField'
-#    name = 'checkout'
--- checkout/models.py
+++ checkout/models.py
@@ -67,7 +67,7 @@ class OrderLineItem(models.Model):
         Override the original save method to set the lineitem total
         and update the order total.
         """
-        self.lineitem_total = self.product.price * self.quantity
+        self.lineitem_total = self.product.product_price * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
