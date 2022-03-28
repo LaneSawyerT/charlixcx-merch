754bb279e56f3c5f930112f249e594e36ae414fb
LaneSawyerT <Lanefair@gmail.com>
2022-03-28 12:56:05 +0000

    Added profile app and functionality to urls, models

--- charlixcx_merch/settings.py
+++ charlixcx_merch/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'merchandise',
     'bag',
     'checkout',
+    'profiles',
 
     # Other
     'crispy_forms',
--- charlixcx_merch/urls.py
+++ charlixcx_merch/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('news/', include('news.urls'), name='news_urls'),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
