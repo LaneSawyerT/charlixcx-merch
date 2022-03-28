4e764185e9cfc421965ffd9da8b91a2a5b1cadfb
LaneSawyerT <Lanefair@gmail.com>
2022-03-28 09:24:10 +0000

    Completed webhook functions

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -82,7 +82,7 @@ class StripeWH_Handler:
                     stripe_pid=pid,
                 )
                 for item_id, item_data in json.loads(bag).items():
-                    product = Merch.objects.get(id=item_id)
+                    product = Product.objects.get(id=item_id)
                     if isinstance(item_data, int):
                         order_line_item = OrderLineItem(
                             order=order,
