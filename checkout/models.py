b407ceaa96ae43caae4d690a19df0d43b3c7daf5
LaneSawyerT <Lanefair@gmail.com>
2022-03-28 01:40:58 +0000

    Added functionality checkout in views, webhooks and webhook handler. Have issue on line 52 in views to be corrected.

--- checkout/models.py
+++ checkout/models.py
@@ -22,19 +22,21 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
-        will generate a random, unique order number using UUID
+        Generate a random, unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
         """
-        Update total each time a line item is added,
+        Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
--- checkout/views.py
+++ checkout/views.py
@@ -9,7 +9,7 @@ from merchandise.models import Merch
 from bag.contexts import bag_contents
 
 import stripe
-
+import json
 
 @require_POST
 def cache_checkout_data(request):
@@ -48,7 +48,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[1]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Merch.objects.get(id=item_id)
@@ -85,7 +89,7 @@ def checkout(request):
         bag = request.session.get('bag', {})
         if not bag:
             messages.error(request, "There's nothing in your bag at the moment")
-            return redirect(reverse('products'))
+            return redirect(reverse('merchandise'))
 
         current_bag = bag_contents(request)
         total = current_bag['grand_total']
