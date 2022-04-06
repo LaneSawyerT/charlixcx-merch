a418c7dce5317d95e76b19c5050f5050ec7c2ba2
LaneSawyerT <Lanefair@gmail.com>
2022-04-06 11:23:29 +0000

    Update view to reflect new like format

--- news/views.py
+++ news/views.py
@@ -29,8 +29,9 @@ class PostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = NewsPost.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
+        user = get_object_or_404(UserProfile, user=request.user)
         liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
+        if post.likes.filter(id=user.id).exists():
             liked = True
 
         return render(
